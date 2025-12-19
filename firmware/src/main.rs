63f8abe1068ac77a15d4f5f74ad9097fb271b651
3nt3 <gott@3nt3.de>
2025-12-19 22:15:43 +0100

    make it build
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -10,24 +10,20 @@ use esp_hal::{
     gpio::{Level, Output},
     timer::timg::TimerGroup,
 };
-use esp_hal_embassy::main;
 use esp_println::println;
+use esp_rtos::main;
 
 #[main]
 async fn main(_spawner: Spawner) {
     esp_println::logger::init_logger_from_env();
 
     let mut config = esp_hal::Config::default();
-    config.cpu_clock = CpuClock::max();
     let peripherals = esp_hal::init(config);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
 
-    let mut led = Output::new(peripherals.GPIO17, Level::High);
     loop {
         println!("Hello, World!");
-        led.toggle();
         Timer::after_millis(1_000).await;
     }
 }
